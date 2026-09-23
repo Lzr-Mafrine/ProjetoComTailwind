4bafad4b7326fdd5bfb19bec51c9d978a31b3097
Aluno <e023aluno@outlook.com>
2026-09-23 19:19:18 -0300

    Correção do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,21 @@
 export default function Header() {
-  <header className="flex items-center justify-between p-4">
-    <div className=" mx-auto flex maw-w-6x1 items-center justify-between px-6 py-4">
-      <h1 className=" text-2x1 font-bold text-yellow-400">
-        &lt;/&gt; Desvendando o código
-      </h1>
+  return (
+    <header className="bg-black text-white">
+      <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
+        <h1 className="text-2xl font-bold text-yellow-400">
+          &lt;/&gt; Desvendando o código
+        </h1>
 
-      <nav className="hidden gap-6 md:flex">
-        <a href="#inicio" className="transition hover:text-yellow-400">
-          Início
-        </a>
+        <nav className="hidden gap-6 md:flex">
+          <a href="#inicio" className="transition hover:text-yellow-400">Início</a>
+          <a href="#recursos" className="transition hover:text-yellow-400">Recursos</a>
+          <a href="#sobre" className="transition hover:text-yellow-400">Sobre</a>
+        </nav>
 
-        <a href="#recursos" className="transition hover:text-yellow-400">
-          Recursos
-        </a>
-
-        <a href="#recursos" className="transition hover:text-yellow-400">
-          Sobre
-        </a>
-      </nav>
-
-      <button
-        className="
-        rounded-1g
-        bg-yellow-400
-        px-4 py-2
-        font-semibold
-        text black
-        transition
-        hover:bg-yellow-300"
-      >
-        Começar
-      </button>
-    </div>
-  </header>;
+        <button className="rounded-lg bg-yellow-400 px-4 py-2 font-semibold text-black transition hover:bg-yellow-300">
+          Começar
+        </button>
+      </div>
+    </header>
+  );
 }
